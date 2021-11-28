864f58608842e426f0af3c5a84602688af015816
kettl96 <kettl110e4@gmail.com>
2021-11-29 01:19:50 +0300

    test1

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <a>home</a>
+      <a>news</a>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>React</li>
+        <li>js</li>
+        <li>js</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Technologies;
