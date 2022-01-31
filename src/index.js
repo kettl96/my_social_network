706481294df0f1a56068674176d0e264fcbd089e
kettl96 <kettl110e4@gmail.com>
2022-01-31 23:22:20 +0300

    Revert "feat: react-redux lub"

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/reduxStore'
-import { Provider } from 'react-redux';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-         <Provider store={store} 
+         <App store={store} 
          state={state} dispatch={store.dispatch.bind(store)} />
     </BrowserRouter>, document.getElementById('root')
   );
