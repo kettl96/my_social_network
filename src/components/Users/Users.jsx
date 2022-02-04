a1182c6f58e448abf1e96d1e6b1733ce3f4c3c72
kettl96 <kettl110e4@gmail.com>
2022-02-04 10:26:23 +0300

    user page number-55

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,36 @@ import * as axios from 'axios';
 let ava = 'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small/beautiful-latin-woman-avatar-character-icon-free-vector.jpg';
 
 class Users extends React.Component {
-
-  constructor(props) {
-    super(props);
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
       .then(response => {
         this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
       });
   }
 
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+    .then(response => {
+      this.props.setUsers(response.data.items);
+    });
+  }
+
   render() {
+
+    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= 10; i++) {
+      pages.push(i);
+    }
     return <div>
+      <div>
+        {pages.map(p => {
+         return <span className={this.props.currentPage === p && styles.selectedPage}
+         onClick={ (e) => { this.onPageChanged(p) }} >{p}</span>
+        })}
+      </div>
       {
         this.props.users.map(u => <div key={u.id}>
           <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,13 +1,19 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+
 
 let initialState = {
   users: [
   //     { id: 1, followed: false, fullName: 'dmitry', status: 'im  bosses ***', location: { city: 'minsk', country: 'belarus' }, photoUrl: 'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small/beautiful-latin-woman-avatar-character-icon-free-vector.jpg' },
   //     { id: 2, followed: true, fullName: 'sasha', status: 'im  bosses ***', location: { city: 'moscow', country: 'belarus' }, photoUrl: 'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small/beautiful-latin-woman-avatar-character-icon-free-vector.jpg' },
   //     { id: 3, followed: false, fullName: 'dmitry', status: 'im  bosses ***', location: { city: 'minsk', country: 'belarus' }, photoUrl: 'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small/beautiful-latin-woman-avatar-character-icon-free-vector.jpg' },
-  ]
+  ],
+  pageSize: 4,
+  totalUsersCount: 0,
+  currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -34,7 +40,13 @@ const usersReducer = (state = initialState, action) => {
         })
       }
     case SET_USERS: {
-      return { ...state, users: [...state.users, ...action.users] }
+      return { ...state, users: action.users }
+    }
+    case SET_CURRENT_PAGE: {
+      return { ...state, currentPage: action.currentPage}
+    }
+    case SET_TOTAL_USERS_COUNT: {
+      return { ...state, totalUsersCount: action.count}
     }
     default:
       return state;
@@ -44,5 +56,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setUsersTotalCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
 
 export default usersReducer;
