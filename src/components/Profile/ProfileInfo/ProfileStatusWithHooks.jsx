615fc724ba1a9d5c65d097c52360f62e15e7609e
kettl96 <kettl110e4@gmail.com>
2022-02-19 17:01:14 +0300

    hook useState

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useState } from 'react';
+import s from "./ProfileInfo.module.css"
+
+const ProfileStatusWithHooks = (props) => {
+
+  let [editMode, setEditMode] = useState(false);
+  let [status, setStatus] = useState(props.status);
+
+  const activatedEditMode = () => {
+    setEditMode(true);
+  }
+  const deactivatedEditMode = () => {
+    setEditMode(false);
+    ;
+    props.updateStatus(status);
+  }
+  const onStatusChange = (e) => {
+    setStatus(e.currentTarget.value);
+  }
+
+  return (
+    <div>
+      {!editMode &&
+        <div className={s.status_bar}>
+          <span
+            onClick={activatedEditMode}
+          >-{props.status}-</span>
+        </div>
+      }
+      {editMode &&
+        <div>
+          <input onChange={onStatusChange} autoFocus
+            onBlur={deactivatedEditMode} type="text"
+            value={status} />
+        </div>
+      }
+    </div>
+  );
+}
+
+
+export default ProfileStatusWithHooks;
