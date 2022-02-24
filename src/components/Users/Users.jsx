a7a4db86e605e7676faa13d1b2eebb19ceef1efa
kettl96 <kettl110e4@gmail.com>
2022-02-24 22:03:23 +0300

    users paginator

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -8,7 +8,7 @@ let Users = ({ currentPage, totalUsersCount, onPageChanged,
   return <div>
     <Paginator currentPage={currentPage}
       onPageChanged={onPageChanged}
-      totalUsersCount={totalUsersCount}
+      totalItemsCount={totalUsersCount}
       pageSize={pageSize} />
     <div>
       {
