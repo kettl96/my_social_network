d6328297d3a804bd4819d6441aa28bfe68c819ee
kettl96 <kettl110e4@gmail.com>
2022-02-25 01:33:01 +0300

    react lazy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -6,19 +6,21 @@ import { Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Setting from './components/Settings/Setting';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+// import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/Login';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/preloader/Preloader';
 
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 
 class App extends Component {
-  componentDidMount() {    
+  componentDidMount() {
     this.props.initializeApp();
   }
 
@@ -27,16 +29,23 @@ class App extends Component {
       return <Preloader />
     }
     return (
-      <div className="app-wrapper">        
+      <div className="app-wrapper">
         <div className="app">
           <HeaderContainer />
           <Navbar />
           <div className="app-wrapper-content">
             <Routes>
+
               <Route path='dialogs/*'
-                element={<DialogsContainer />} />
-              <Route path='profile' element={<ProfileContainer />}>
-                <Route path=":userId" element={<ProfileContainer />} />
+                element={<Suspense fallback={<div>Loading...</div>}>
+                  <DialogsContainer />
+                </Suspense>} />
+              <Route path='profile' element={<Suspense fallback={<div>Loading...</div>}>
+                <ProfileContainer />
+              </Suspense>}>
+                <Route path=":userId" element={<Suspense fallback={<div>Loading...</div>}>
+                  <ProfileContainer />
+                </Suspense>} />
               </Route>
               <Route path='news' element={<News />} />
               <Route path='music' element={<Music />} />
@@ -55,5 +64,5 @@ const mapStateToProps = (state) => ({
   initialized: state.app.initialized
 })
 
-export default connect(mapStateToProps, { initializeApp }) (App);
+export default connect(mapStateToProps, { initializeApp })(App);
 
