51bc10087e912fd09f86682ae0b3c3cd1a66674a
kettl96 <kettl110e4@gmail.com>
2022-02-25 23:21:27 +0300

    upd doc

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import store from './redux/reduxStore'
 import { Provider } from 'react-redux';
 import reactDom from 'react-dom';
 
 reactDom.render(
-  <BrowserRouter>
+  <HashRouter >
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>, document.getElementById('root')
+  </HashRouter>, document.getElementById('root')
 );
