662caf7ba255e6ad273b5687899428b6c413bc1d
kettl96 <kettl110e4@gmail.com>
2022-03-08 02:33:27 +0300

    dialogs-reducer on ts

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -1,3 +1,16 @@
+const ADD_MESSAGE = 'ADD-MESSAGE'
+
+type DialogType = {
+  id: number
+  name: string
+  img: string
+}
+type MessageType = {
+  id: number
+  message: string
+  isMine: boolean
+}
+
 let initialState = {
   dialogs: [
     { id: 1, name: 'Dimych', img: 'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg' },
@@ -5,24 +18,26 @@ let initialState = {
     { id: 3, name: 'sasha', img: 'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg' },
     { id: 4, name: 'hover', img: 'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg' },
     { id: 5, name: 'hren', img: 'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg' },
-  ],
-
+  ] as Array<DialogType>,
   messages: [
     { id: 1, message: 'hi', isMine: true },
     { id: 2, message: 'how is you day', isMine: false },
     { id: 3, message: 'yo', isMine: true },
     { id: 4, message: 'jjhfhjbhddjfhjdh', isMine: false },
     { id: 5, message: 'jskfdjfjklsnfjlksdnfsnjdnfjksndj', isMine: true }
-  ]
+  ] as Array<MessageType>
 };
 
-const dialogsReducer = (state = initialState, action) => {
+export type InitialStateType = typeof initialState
+
+const dialogsReducer = (state = initialState, action: any): InitialStateType => {
 
   switch (action.type) {
-    case 'ADD-MESSAGE':
+    case ADD_MESSAGE:
       let newMessage = {
         id: state.messages[state.messages.length - 1].id + 1,
         message: action.newMessageBody,
+        isMine: false
       };
       return {
         ...state,
@@ -33,10 +48,13 @@ const dialogsReducer = (state = initialState, action) => {
   }
 
 }
-
-export const addMessageActionCreator = (newMessageBody) => {
+type addMessageActionCreatorType = {
+  type: typeof ADD_MESSAGE
+  newMessageBody: string
+}
+export const addMessageActionCreator = (newMessageBody: string): addMessageActionCreatorType => {
   return {
-    type: 'ADD-MESSAGE', newMessageBody
+    type: ADD_MESSAGE, newMessageBody
   }
 }
 
