d89bf4a0c67eb22c82524ad10630211dc551e07c
kettl96 <kettl110e4@gmail.com>
2022-03-13 17:45:08 +0300

    ts

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,5 +1,6 @@
 import { profileAPI, usersAPI } from "../api/api";
 import { stopSubmit } from 'redux-form';
+import { PhotosType, PostType, ProfileType } from "../types/types";
 
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
@@ -7,34 +8,6 @@ const SET_STATUS = 'SET_STATUS';
 const DELETE_POST = 'DELETE_POST';
 const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
-type PostType = {
-  id: number,
-  message: string,
-  likesCount: number
-}
-
-type PhotosType = {
-  small: string | null
-  large: string | null
-}
-type ContactsType = {
-  github: string
-  vk: string
-  facebook: string
-  instagram: string
-  twitter: string
-  website: string
-  youtube: string
-  mainLink: string
-}
-type ProfileType = {
-  userId: number
-  lookingForAJob: boolean
-  lookingForAJobDescription: string
-  fullName: string
-  contacts: ContactsType
-  photos: PhotosType
-}
 
 let initialState = {
   posts: [
